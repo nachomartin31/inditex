110b6ed4f4052a56fe7cb86a95855f33a8533ba9
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-10 23:08:49 +0200

    Add some styles

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,10 @@
 import MobilesList from '../components/MobilesList';
+import '../styles/mobileList.scss';
 
 function Home() {
   return (
     <div>
+      <h2 className="mobileList__heading">MobilesList</h2>
       <MobilesList />
     </div>
   );
