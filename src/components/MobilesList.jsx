ccfc9e1bbad9042b4e89d953bd86436248f98a09
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-10 20:20:32 +0200

    Add miniature component

--- src/components/MobilesList.jsx
+++ src/components/MobilesList.jsx
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import MobileMiniature from './MobileMiniature';
 
 function MobilesList() {
   const filteredMobilesList = useSelector((state) => state.filteredMobilesList.filteredMobilesList);
@@ -6,7 +8,7 @@ function MobilesList() {
   return (
     <div>
       <h1>MobilesList</h1>
-      {filteredMobilesList.map((mobile) => <p key={mobile.id}>{mobile.model}</p>)}
+      {filteredMobilesList.map((mobile) => <Link key={mobile.id} to={`/${mobile.id}`}><MobileMiniature mobile={mobile} /></Link>)}
     </div>
   );
 }
