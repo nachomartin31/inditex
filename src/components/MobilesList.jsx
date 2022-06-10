3e4fc4d1f0e8919a42907e4f429e0d883bd8db2d
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-10 17:37:01 +0200

    Refactor component:
    - Delete state management
    - Render filtered items instead of entire list

--- src/components/MobilesList.jsx
+++ src/components/MobilesList.jsx
@@ -1,22 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { loadMobilesList } from '../redux/slices/mobilesList';
+import { useSelector } from 'react-redux';
 
 function MobilesList() {
-  const mobilesList = useSelector((state) => state.mobilesList.mobilesList);
-  const dispatch = useDispatch();
-  async function loadMobiles() {
-    const { data } = await axios.get(`${process.env.REACT_APP_URL}api/product`);
-    dispatch(loadMobilesList(data));
-  }
-  useEffect(() => {
-    loadMobiles();
-  }, []);
+  const filteredMobilesList = useSelector((state) => state.filteredMobilesList.filteredMobilesList);
+
   return (
     <div>
       <h1>MobilesList</h1>
-      {mobilesList.map((mobile) => <p key={mobile.id}>{mobile.model}</p>)}
+      {filteredMobilesList.map((mobile) => <p key={mobile.id}>{mobile.model}</p>)}
     </div>
   );
 }
