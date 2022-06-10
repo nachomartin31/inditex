15d2e96ed547b48bb6c7d23770819f8ae9fee832
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-10 20:43:28 +0200

    Create mobile list styles module

--- src/components/MobilesList.jsx
+++ src/components/MobilesList.jsx
@@ -1,14 +1,17 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import MobileMiniature from './MobileMiniature';
+import '../styles/mobileList.scss';
 
 function MobilesList() {
   const filteredMobilesList = useSelector((state) => state.filteredMobilesList.filteredMobilesList);
 
   return (
-    <div>
-      <h1>MobilesList</h1>
-      {filteredMobilesList.map((mobile) => <Link key={mobile.id} to={`/${mobile.id}`}><MobileMiniature mobile={mobile} /></Link>)}
+    <div className="mobileList">
+      <h2 className="mobileList__heading">MobilesList</h2>
+      <section className="mobileList__content">
+        {filteredMobilesList.map((mobile) => <Link key={mobile.id} to={`/${mobile.id}`}><MobileMiniature mobile={mobile} /></Link>)}
+      </section>
     </div>
   );
 }
