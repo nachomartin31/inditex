e1e8d7379a70c1c23914a07288c1172dc89f59f7
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-11 22:19:23 +0200

    Add cart slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import mobilesListReducer from './slices/mobilesList';
 import filteredMobilesListReducer from './slices/filteredMobilesList';
 import currentMobileReducer from './slices/currentMobile';
+import cartSliceReducer from './slices/cartSlice';
 
 // eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
   reducer: {
     mobilesList: mobilesListReducer,
     filteredMobilesList: filteredMobilesListReducer,
-    currentMobile: currentMobileReducer
+    currentMobile: currentMobileReducer,
+    cart: cartSliceReducer
   }
 });
