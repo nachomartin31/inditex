b34223d18f86886f2bcdb6cfe888bd0d640d336b
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-13 16:33:58 +0200

    Remove cookie provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { CookiesProvider } from 'react-cookie';
 import App from './App';
 import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </CookiesProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
